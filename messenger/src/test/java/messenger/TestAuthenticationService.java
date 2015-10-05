7ec219fd660ef343b028d9b06702001abfabfca5
Chejerla Karthik <chejerlakarthik@gmail.com>
2015-10-05 19:22:34 +0530

    Created an empty test case

---
+++ messenger/src/test/java/messenger/TestAuthenticationService.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package messenger;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author 539471
+ *
+ */
+public class TestAuthenticationService {
+
+	/**
+	 * Test method for {@link org.javabrains.messenger.service.HttpBasicAuthenticationService#doAuthentication(java.lang.String)}.
+	 */
+	@Test
+	public void testDoAuthentication() {
+		
+		
+	}
+
+}
